1733814dd4f67675e4b719d05100f667218d54a8
J Cole Morrison <me@jcolemorrison.com>
2022-03-18 11:00:03 -0700

    dc2 albs and all initial security groups

--- ec2-dc2.tf
+++ ec2-dc2.tf
@@ -1,13 +1,13 @@
-resource "aws_instance" "dc2_bastion" {
+resource "aws_instance" "bastion_dc2" {
   ami                         = var.use_latest_ami ? data.aws_ssm_parameter.ubuntu_1804_ami_id.value : var.ami_id
   instance_type               = "t3.micro"
   key_name                    = var.ec2_key_pair_name
-  vpc_security_group_ids      = [aws_security_group.dc2_bastion.id]
+  vpc_security_group_ids      = [aws_security_group.bastion_dc2.id]
   subnet_id                   = aws_subnet.dc2_public[0].id
   associate_public_ip_address = true
 
   tags = merge(
-    { "Name" = "${var.main_project_tag}-dc2-bastion" },
+    { "Name" = "${var.main_project_tag}-bastion-dc2" },
     { "Project" = var.main_project_tag }
   )
 }
