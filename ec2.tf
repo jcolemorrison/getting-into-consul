93e8f1625a4958d5dc8d447da687650cbf04a572
J Cole Morrison <me@jcolemorrison.com>
2022-03-18 10:44:30 -0700

    mesh gateways and userdata

--- ec2.tf
+++ ec2.tf
@@ -15,3 +15,29 @@ resource "aws_instance" "bastion" {
     { "Project" = var.main_project_tag }
   )
 }
+
+resource "aws_instance" "mesh_gateway" {
+  ami                         = var.use_latest_ami ? data.aws_ssm_parameter.ubuntu_1804_ami_id.value : var.ami_id
+  instance_type               = "t3.micro"
+  vpc_security_group_ids      = [aws_security_group.mesh_gateway.id]
+  subnet_id                   = aws_subnet.private[0].id
+  key_name                    = var.ec2_key_pair_name
+
+  iam_instance_profile = aws_iam_instance_profile.consul_instance_profile.name
+
+  user_data = base64encode(templatefile("${path.module}/scripts/mesh_gateway.sh", {
+    PROJECT_TAG   = "Project"
+    PROJECT_VALUE = var.main_project_tag
+    GOSSIP_KEY = random_id.gossip_key.b64_std
+    CA_PUBLIC_KEY = tls_self_signed_cert.ca_cert.cert_pem
+    CLIENT_PUBLIC_KEY = tls_locally_signed_cert.mesh_gateway_signed_cert.cert_pem
+    CLIENT_PRIVATE_KEY = tls_private_key.mesh_gateway_key.private_key_pem
+  }))
+
+  tags = merge(
+    { "Name" = "${var.main_project_tag}-mesh-gateway" },
+    { "Project" = var.main_project_tag }
+  )
+
+  depends_on = [aws_nat_gateway.nat]
+}
