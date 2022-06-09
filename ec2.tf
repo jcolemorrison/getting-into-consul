e28d62862f1c68afd34362bebcb6803bad46d96d
J Cole Morrison <me@jcolemorrison.com>
2022-06-09 11:22:20 -0700

    cts instance profile

--- ec2.tf
+++ ec2.tf
@@ -47,3 +47,33 @@ resource "aws_instance" "consul_server" {
 
   depends_on = [aws_nat_gateway.nat]
 }
+
+resource "aws_instance" "consul_client_cts" {
+  ami                    = var.use_latest_ami ? data.aws_ssm_parameter.ubuntu_1804_ami_id.value : var.ami_id
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.consul_client.id]
+  subnet_id              = aws_subnet.private[0].id
+  key_name               = var.ec2_key_pair_name
+
+  iam_instance_profile = aws_iam_instance_profile.consul_cts_instance_profile.name
+
+  private_ip = local.server_private_ips[count.index]
+
+  user_data = base64encode(templatefile("${path.module}/scripts/server.sh", {
+    PROJECT_TAG        = "Project"
+    PROJECT_VALUE      = var.main_project_tag
+    BOOTSTRAP_NUMBER   = var.server_desired_count
+    GOSSIP_KEY         = random_id.gossip_key.b64_std
+    CA_PUBLIC_KEY      = tls_self_signed_cert.ca_cert.cert_pem
+    SERVER_PUBLIC_KEY  = tls_locally_signed_cert.server_signed_cert[count.index].cert_pem
+    SERVER_PRIVATE_KEY = tls_private_key.server_key[count.index].private_key_pem
+    BOOTSTRAP_TOKEN    = random_uuid.consul_bootstrap_token.result
+  }))
+
+  tags = merge(
+    { "Name" = "${var.main_project_tag}-server" },
+    { "Project" = var.main_project_tag }
+  )
+
+  depends_on = [aws_nat_gateway.nat]
+}
--- iam.tf
+++ iam.tf
@@ -1,9 +1,4 @@
-resource "aws_iam_role" "consul_instance" {
-  name_prefix        = "${var.main_project_tag}-role-"
-  assume_role_policy = data.aws_iam_policy_document.instance_trust_policy.json
-}
-
-## Consul Instance Trust Policy
+## General Instance Trust Policy
 data "aws_iam_policy_document" "instance_trust_policy" {
   statement {
     effect = "Allow"
@@ -15,6 +10,12 @@ data "aws_iam_policy_document" "instance_trust_policy" {
       "sts:AssumeRole"
     ]
   }
+}
+
+# Consul Instance IAM Role, Policy, and Profile
+resource "aws_iam_role" "consul_instance" {
+  name_prefix        = "${var.main_project_tag}-role-"
+  assume_role_policy = data.aws_iam_policy_document.instance_trust_policy.json
 }
 
 # Consul Instance Permissions Policy
@@ -40,4 +41,18 @@ resource "aws_iam_role_policy" "consul_instance_policy" {
 resource "aws_iam_instance_profile" "consul_instance_profile" {
   name_prefix = "${var.main_project_tag}-instance-profile-"
   role        = aws_iam_role.consul_instance.name
+}
+
+# Consul CTS IAM Role, Policy, and Profile
+
+resource "aws_iam_role" "consul_cts_instance" {
+  name_prefix        = "${var.main_project_tag}-cts-role-"
+  assume_role_policy = data.aws_iam_policy_document.instance_trust_policy.json
+  # includes the needed "ec2:DescribeInstances" permissions
+  managed_policy_arns = ["arn:aws:iam::aws:policy/ElasticLoadBalancingFullAccess"]
+}
+
+resource "aws_iam_instance_profile" "consul_cts_instance_profile" {
+  name_prefix = "${var.main_project_tag}-cts-instance-profile-"
+  role        = aws_iam_role.consul_cts_instance.name
 }
